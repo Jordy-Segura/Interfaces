ccda65a6464066ef348d5aa8cb7fd1a3b4cc71c4
jordy <marcelo2005jmsp@gmail.com>
2025-01-16 14:27:41 -0500

    interfaz Jordy

---
+++ src/main/java/ec/edu/espoch/interfaces/Interfaces.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.interfaces;
+
+/**
+ *
+ * @author jordy
+ */
+public class Interfaces {
+
+    public static void main(String[] args) {
+        Interfaz1 obInterfaz = new Interfaz1();
+        obInterfaz.setVisible(true);
+    }
+}
